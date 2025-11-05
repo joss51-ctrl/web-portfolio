e87fb2d1f6ddd66c7cd99f140ce16a709dff4f0f
Yechiel Josey <yechieljo@gmail.com>
2025-11-06 04:51:04 +0700

    add movie project

--- src/data.js
+++ src/data.js
@@ -33,14 +33,31 @@ import Proyek7 from "/assets/proyek/mern-1.png";
 import Project7_detail from "/assets/proyek/mern-hover.png";
 import Project77_detail from "/assets/proyek/mern-edit.png";
 
+import Proyek8 from "/assets/proyek/Cinema-1.png";
+import Project8_detail from "/assets/proyek/Cinema-2.png";
+import Project88_detail from "/assets/proyek/Cinema-3.png";
+import Project888_detail from "/assets/proyek/Cinema-4.png";
+
 export const listProyek = [
+    {
+    id: 8,
+    images: [Proyek8, Project8_detail, Project88_detail, Project888_detail],
+    nama: "TMDB Movie Website",
+    desk: "A responsive front-end application built with React and Tailwwindcss to browse and discover movies and TV shows. Powered by the TMDb API.",
+    tools: ["React", "TailwindCSS"],
+    dad: "100",
+    link:"https://github.com/joss51-ctrl/Cinemaplex",
+    linkweb:"https://cinemaplex-2ek2mc8u9-joseys-projects-d921b50e.vercel.app/",
+    category:"WEB"
+  },
+  
   {
     id: 7,
     images: [Proyek7, Project7_detail, Project77_detail],
     nama: "MERN Stack Library",
     desk: "Built a Library Management System using the MERN stack, Using CRUD operations and RESTful APIs integration.",
     tools: ["React", "TailwindCSS", "MongoDB", "Node.js", "Express.js"],
-    dad: "100",
+    dad: "200",
     link:"https://github.com/joss51-ctrl/MERN-Library",
     linkweb:"https://mern-library-hlxn.onrender.com",
     category:"WEB"
@@ -51,7 +68,7 @@ export const listProyek = [
     nama: "Simple CRUD User Management",
     desk: "Built a full-stack User Management web with an interactive dashboard that visualizes gender distribution in real time. Using CRUD operations and RESTful API integration.",
     tools: ["React", "TailwindCSS", "SQL", "Node.js", "Express.js"],
-    dad: "100",
+    dad: "300",
     link:"https://github.com/joss51-ctrl/SimpleCRUD",
     category:"WEB"
   },
@@ -61,7 +78,7 @@ export const listProyek = [
     nama: "Food Catering Website",
     desk: "Developed a simple website using HTML, CSS, and JavaScript. JavaScript is used to handle form validation and CSS for the design.",
     tools: ["HTML", "CSS", "Javascript"],
-    dad: "200",
+    dad: "400",
     link:"https://github.com/joss51-ctrl/food_catering_website",
     category:"WEB"
   },
@@ -71,7 +88,7 @@ export const listProyek = [
     nama: "Integrating Next Word Prediction Into Transcription",
     desk: "A Transcription system with a next word prediction. Each word in the transcript is evaluated using entropy to measure confidence. If confidence is low, the system triggers Next Word Prediction to suggest a more contextually appropriate word.",
     tools: ["Python", "Streamlit"],
-    dad: "300",
+    dad: "500",
     link: "https://github.com/joss51-ctrl/transcription-nextword-prediction",
     category:"ML/AI"
   },
@@ -81,7 +98,7 @@ export const listProyek = [
     nama: "Live Transcription with Noise and Echo Cancellation",
     desk: "A real-time Transcription System that converts speech to text using noise reduction and echo cancellation. The app features interface where users can log in, access their transcription history, and export transcripts to .txt.",
     tools: ["Python", "React", "Streamlit"],
-    dad: "400",
+    dad: "600",
     link:"https://github.com/Angga-laksana/Noise-Resistant-Live-Transcription",
     category:"ML/AI"
   },
@@ -91,7 +108,7 @@ export const listProyek = [
     nama: "Diabetes and Heart Disease Predicition",
     desk: "A machine learning application that predicts the risk of diabetes and heart disease based on medical features. The system integrates two classification models into one platform, allowing users to run both risk assessments through an interactive Streamlit interface.",
     tools: ["Python", "Streamlit"],
-    dad: "500",
+    dad: "700",
     link:"https://github.com/joss51-ctrl/Diabetes_HeartDiseasePrediction",
     category:"ML/AI"
   },
